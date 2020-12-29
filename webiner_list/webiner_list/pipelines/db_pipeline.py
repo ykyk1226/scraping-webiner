7aeb11bb3d8be20fbf379198648c5dc0d7e328f5
Yoshiaki Kanazawa <k.yoshiaki1226@gmail.com>
2020-12-29 17:24:12 +0900

    日本語の文字化け対応

--- webiner_list/webiner_list/pipelines/db_pipeline.py
+++ webiner_list/webiner_list/pipelines/db_pipeline.py
@@ -3,7 +3,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import scrapy
@@ -19,13 +18,16 @@ class DbPipeline:
         password = 'P@ssword'
         self.conn = pyodbc.connect('DRIVER={%s};SERVER=%s;PORT=%s;DATABASE=%s;UID=%s;PWD=%s' % (driver, server, port, database, username, password))
 
+        delete_sel = "DELETE FROM webiner_lists"
+        curs = self.conn.cursor()
+        curs.execute(delete_sel)
+        self.conn.commit()
+
     def close_spider(self, spider: scrapy.Spider):
         self.conn.close()
 
     def process_item(self, item: scrapy.Item, spider: scrapy.Spider):
         curs = self.conn.cursor()
-        delete_sel = "DELETE FROM webiner_lists"
-        curs.execute(delete_sel)
         insert_sql = "INSERT INTO webiner_lists VALUES (CAST(NEXT VALUE FOR WebinerListsSequence AS VARCHAR), ?, ?, ?, ?)"
         curs.execute(insert_sql, (item['title'], item['url'], item['date'].strftime('%Y/%m/%d %H:%M:%S'), item['category_id']))
         self.conn.commit()
