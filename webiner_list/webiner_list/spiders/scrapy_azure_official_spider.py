b3664a67c3a5a9c7e1e28e9aeb5a606ff074c37e
Yoshiaki Kanazawa <k.yoshiaki1226@gmail.com>
2021-08-06 23:12:16 +0900

    sub_parse関数削除

--- webiner_list/webiner_list/spiders/scrapy_azure_official_spider.py
+++ webiner_list/webiner_list/spiders/scrapy_azure_official_spider.py
@@ -2,8 +2,6 @@ import scrapy
 from webiner_list.items import AzureOfficialItem
 import pytz
 from datetime import datetime, timedelta
-from urllib.request import urlopen
-#from bs4 import BeautifulSoup
 
 class ScrapyAzureOfficialSpider(scrapy.Spider):
     name = 'scrapy_azure_official'
@@ -11,29 +9,10 @@ class ScrapyAzureOfficialSpider(scrapy.Spider):
     start_urls = ['https://azure.microsoft.com/ja-jp/community/events/?Page=1']
     source_site_id = "1"
 
-    # webiner詳細ページから終了時間を取得
-    # def sub_parse(self, webiner_url):
-    #     html = urlopen(webiner_url)
-    #     data = html.read()
-    #     html = data.decode('utf-8')
-    #     soup = BeautifulSoup(html, "html.parser")
-    #     print(html)
-    #     iframe_src = soup.select_one("#myIframe").attrs["src"]
-    #     iframe_html = urlopen(iframe_src)
-    #     iframe_data = iframe_html.read()
-    #     iframe_html = iframe_data.decode('utf-8')
-    #     iframe_soup = BeautifulSoup(iframe_html, "html.parser")
-    #     print(iframe_html)
-    #     print(iframe_soup.find_all("span", class_="date"))
-    #     return {
-    #         'end_date' : soup.select('div#progDateTime.dateTime.mktoText'),
-    #     }
-
     def parse(self, response):
         # webiner一覧を取得
         for event in response.css('.row.event-item'):
             webiner_url = event.css('.column.medium-11 a::attr(href)').extract_first().strip()
-            #sub_response = self.sub_parse(webiner_url)
 
             # webiner開催時間を日本時間に修正
             end_date = event.css('.column.medium-11 span::text').extract_first().strip()
